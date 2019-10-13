42e353af8da6ac114ea74e5789a5ea86ffc610a1
Ethan Pollack <Ethan.Pollack@GMR.local>
2019-10-13 15:01:29 -0700

    Added SkipList

---
+++ SkipList/SkipList/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkipList
+{
+    public class Node<T> where T : IComparable
+    {
+        private List<Node<T>> _neighbors;
+
+        public Node()
+        {
+
+        }
+    }
+}
